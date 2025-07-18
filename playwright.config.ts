e5c640031006d233d01218ece1d5314b74f44fa0
Aidan Owens <whiskeyfoxtrot@tamu.edu>
2025-07-18 12:55:35 -0500

    fix: Extend test timeout to 60 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 60*1000, 
+  /*Sets the extends the timeout durations*/
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
